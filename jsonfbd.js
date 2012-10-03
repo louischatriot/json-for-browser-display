9ec48986ebdd8da81c943c0c3b20958b65a7d604
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-03 17:04:10 +0200

    Able to get set the content of the DOM with jquery

--- jsonfbd.js
+++ jsonfbd.js
@@ -1,59 +1,31 @@
 /*
- * "M command" bookmarklet, gives you the same functionality as the vim 'm' command but on a webpage
- * May conflict with websites using keys, such as Github, but you can customize the bindings so that it works with your
- * favourite websites
+ * Bookmarklet that gets the json being displayed and returns a nicely formatted (and indented) JSON
  * Author: Louis Chatriot
  */
 
 
-var mcommandplugin = mcommandplugin || {};
-mcommandplugin.i;
-mcommandplugin.currentTop = {};
-for (mcommandplugin.i = 65; mcommandplugin.i <= 90; mcommandplugin.i += 1) { mcommandplugin.currentTop[mcommandplugin.i] = null; }
+var jfbd = jfbd || {};
+jfbd.indentLevel = "  ";
 
-mcommandplugin.setCommandPressed = false;
-mcommandplugin.getCommandPressed = false;
-mcommandplugin.setCommandKey = 77;   // Use 'm' as the command key, as in vim. May need to change since Github uses it, too ...
-mcommandplugin.getCommandKey = 222;
+jfbd.run = function($) {
+  var contents, prop;
+
+  console.log("jQuery and jfbd bookmarklet loaded");
+
+  // Works with Chrome with which I develop, not tested on firefox
+  contents = JSON.parse($('pre').html());
 
 
-mcommandplugin.run = function($) {
 
-  var resetPressedState = function() { mcommandplugin.setCommandPressed = false; mcommandplugin.getCommandPressed = false; };
+console.log(contents);
 
-  mcommandplugin.$body = $('body');
-  mcommandplugin.$inputs = $('input');
 
-  $(document).bind('keydown', function(e) {
-    if (mcommandplugin.$inputs.is(':focus')) {
-      resetPressedState();   // Do nothing if user is typing into an input box
-      return;
-    }
 
-    if (mcommandplugin.setCommandPressed) {
-      if ((e.keyCode >= 65) && (e.keyCode <= 90)) {
-        mcommandplugin.currentTop[e.keyCode] = mcommandplugin.$body.scrollTop();
-      }
 
-      resetPressedState();
-    } else if (mcommandplugin.getCommandPressed) {
-      if ((e.keyCode >= 65) && (e.keyCode <= 90)) {
-        if (mcommandplugin.currentTop[e.keyCode] !== null) {
-          mcommandplugin.$body.scrollTop(mcommandplugin.currentTop[e.keyCode]);
-        }
-      }
 
-      resetPressedState();
-    } else {
-      if (e.keyCode === mcommandplugin.setCommandKey) {
-        mcommandplugin.setCommandPressed = true;
-      } else if (e.keyCode === mcommandplugin.getCommandKey) {
-        mcommandplugin.getCommandPressed = true;
-      } else {
-        resetPressedState();
-      }
-    }
-  });
+//var newDoc = document.open("text/html", "replace");
+//newDoc.close();
+//$('body').html('dsdfsfsf<br>sdssdsdsdfZZZ');
 
 };
 
@@ -61,8 +33,6 @@ mcommandplugin.run = function($) {
 
 // Launch the bookmarklet
 (function (app) {
-  console.log("M command bookmarklet launched");
-
   //Get jQuery if the current page doesn't have it
   if ( typeof jQuery === 'undefined' || jQuery.fn.jquery.substring(0,3) !== '1.7') {
 
@@ -73,11 +43,11 @@ mcommandplugin.run = function($) {
       app.run(jQuery);
     }
 
-
     document.body.appendChild(fileref);
+
   } else {
     app.run(jQuery);
   }
-}(mcommandplugin));
+}(jfbd));
 
 
